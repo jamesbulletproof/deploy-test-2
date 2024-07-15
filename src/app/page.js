248a27cc52a74558050b3517451bb411ad1c76ec
jamesbulletproof <113986445+jamesbulletproof@users.noreply.github.com>
2024-07-15 13:55:30 +0100

    commit message

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        Hi there this is from James
+        Hi there this is from James MAckenzie who has updated
       </div>
     </main>
   );
